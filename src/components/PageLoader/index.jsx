f903c928ab0daa9ebc3eb3fb58cb55070e99606f
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-06-19 18:23:22 +0800

    Created a minimal landing page. Testing color schemes and designs

--- src/components/PageLoader/index.jsx
+++ src/components/PageLoader/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import LoadingIcon from '@/components/common/LoadingIcon';
 
 const PageLoader = () => {
   return (
-    <div>Page Loading...</div>
-  )
-}
+    <div className="w-screen h-screen flex items-center justify-center">
+      <LoadingIcon />
+    </div>
+  );
+};
 
 export default PageLoader;
--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeaf, faArrowCircleRight, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landiing</h1>
+    <div className="w-screen h-screen grid grid-cols-1 grid-rows-[15%_85%] bg-[#FFF6E9]">
+        <div className='w-full grid grid-cols-3 grid-rows-1 items-center'>
+          <h1 className='ml-8 font-serif text-2xl'>
+            NaturaFund
+            <FontAwesomeIcon icon={faLeaf} className='text-[#0D9276] ml-3' />
+          </h1>
+          <ul className='w-full flex items-center justify-evenly text-[#004536] [&>*]:cursor-pointer'>
+            <li>
+              Solutions
+              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
+            </li>
+            <li>
+              Solutions
+              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
+            </li>
+            <li>
+              Solutions
+              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
+            </li>
+          </ul>
+          <button className='w-fit h-fit p-2 px-8 rounded-3xl bg-[#BBE2EC] text-[#203949] justify-self-end mr-14'>
+            Login
+          </button>
+        </div>
+        <div className='w-full h-full flex items-center justify-center'>
+          <div className='w-5/6 h-5/6 grid grid-cols-1 grid-rows-[40%_10%_45%] place-items-center bg-[url("./pic1.jpg")] bg-center bg-cover bg-no-repeat rounded-2xl'>
+            <h1 className='text-6xl font-serif font-bold text-[#f6ff4c]'>
+              Invest in Nature
+            </h1>
+            <p className='text-[#f6ff4c] text-3xl text-center font-serif'>
+              NaturaFund is a platform that allows you to invest in nature.
+            </p>
+            <FontAwesomeIcon icon={faArrowCircleDown} className='text-[#f6ff4c] text-3xl animate-bounce' />
+            </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Landing;
