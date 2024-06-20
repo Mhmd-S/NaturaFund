ab7d8e865f2bc27cc6a09930c2b1cb377147ded7
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-06-20 16:42:58 +0800

    Made a new style for the landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -4,41 +4,37 @@ import { faLeaf, faArrowCircleRight, faArrowCircleDown } from '@fortawesome/free
 
 const Landing = () => {
   return (
-    <div className="w-screen h-screen grid grid-cols-1 grid-rows-[15%_85%] bg-[#FFF6E9]">
-        <div className='w-full grid grid-cols-3 grid-rows-1 items-center'>
-          <h1 className='ml-8 font-serif text-2xl'>
-            NaturaFund
-            <FontAwesomeIcon icon={faLeaf} className='text-[#0D9276] ml-3' />
-          </h1>
-          <ul className='w-full flex items-center justify-evenly text-[#004536] [&>*]:cursor-pointer'>
-            <li>
-              Solutions
-              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
-            </li>
-            <li>
-              Solutions
-              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
-            </li>
-            <li>
-              Solutions
-              <FontAwesomeIcon icon={faArrowCircleRight} className='ml-2' />
-            </li>
-          </ul>
-          <button className='w-fit h-fit p-2 px-8 rounded-3xl bg-[#BBE2EC] text-[#203949] justify-self-end mr-14'>
-            Login
-          </button>
-        </div>
-        <div className='w-full h-full flex items-center justify-center'>
-          <div className='w-5/6 h-5/6 grid grid-cols-1 grid-rows-[40%_10%_45%] place-items-center bg-[url("./pic1.jpg")] bg-center bg-cover bg-no-repeat rounded-2xl'>
-            <h1 className='text-6xl font-serif font-bold text-[#f6ff4c]'>
-              Invest in Nature
-            </h1>
-            <p className='text-[#f6ff4c] text-3xl text-center font-serif'>
-              NaturaFund is a platform that allows you to invest in nature.
-            </p>
-            <FontAwesomeIcon icon={faArrowCircleDown} className='text-[#f6ff4c] text-3xl animate-bounce' />
-            </div>
-        </div>
+    <div className="relative w-screen h-screen flex flex-col bg-[url(./pic1.jpg)] bg-cover bg-center bg-no-repeat">
+      <div className="h-fit w-full p-2 grid grid-cols-3 grid-rows-1 items-centerrounded-3xl place-self-center justify-self-center text-white backdrop-blur-xl">
+        <h1 className="ml-8 font-serif text-3xl">
+          NaturaFund
+          <FontAwesomeIcon icon={faLeaf} className="text-white ml-3" />
+        </h1>
+        <ul className="w-full flex items-center justify-evenly text-white [&>*]:cursor-pointer">
+          <li>
+            Solutions
+            <FontAwesomeIcon icon={faArrowCircleRight} className="ml-2" />
+          </li>
+          <li>
+            Solutions
+            <FontAwesomeIcon icon={faArrowCircleRight} className="ml-2" />
+          </li>
+          <li>
+            Solutions
+            <FontAwesomeIcon icon={faArrowCircleRight} className="ml-2" />
+          </li>
+        </ul>
+        <button className="w-fit h-fit p-2 px-8 rounded-2xl border-2 border-[#6db5c8] justify-self-end mr-14">
+          Invest Now
+        </button>
+      </div>
+      <div className="relative w-full h-full flex flex-col items-center justify-center bg-center bg-cover bg-no-repeat rounded-2xl text-white">
+        <h1 className="text-6xl font-serif font-bold pb-1 underline backdrop-blur-sm p-5">Invest in Nature</h1>
+        <p className="text-2xl font-serif font-light w-3/5 text-center backdrop-blur-sm p-3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque, urna nec
+          facilisis ultricies, nunc justo gravida ligula, nec venenatis nunc mi ut mi.
+        </p>
+      </div>
     </div>
   );
 };
