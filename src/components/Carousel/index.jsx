3783c60004dc03f83fc4792684daeafc49bfc1bf
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-01 23:44:19 +0800

    Added the remaining images for the Carousel. Refactored the Carousel to use grid.

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -15,7 +15,7 @@ const Carousel = ({ items }) => {
             return (
                 <span
                     key={item.title}
-                    className={`relative flex-1 p-1 rounded-lg bg-slate-600`}
+                    className={`relative flex-1 p-1 rounded-lg bg-slate-600 cursor-pointer`}
                     onClick={() => setIndex(ind)}
                 >
                     <span
@@ -31,9 +31,9 @@ const Carousel = ({ items }) => {
         const currentContent = items[index];
         const content = (
             <>
-                <h3 className="text-2xl font-bold">{currentContent.title}</h3>
-                <div className="text-white p-4">{currentContent.content}</div>
-                <img src={"./" + currentContent.img} />
+                <h3 className="w-full text-2xl font-semibold">{currentContent.title}</h3>
+                <div className="text-white">{currentContent.content}</div>
+                <img className="w-full rounded-b-lg" src={"./" + currentContent.img} />
             </>
         );
 
@@ -41,8 +41,9 @@ const Carousel = ({ items }) => {
     };
 
     return (
-        <div className="w-full flex flex-col items-center justify-center  bg-brand-900 bg-opacity-85 rounded-2xl text-white">
-            <div className="w-full grid grid-flow-col grid-rows-1 gap-1 items-center p-3">
+        <div className="w-full p-4 h-max grid grid-cols-1 grid_flow-rows gap-4 bg-brand-900 bg-opacity-85 rounded-2xl text-white">
+            <h2 className="w-full text-xl font-bold">How Does It Work?</h2>
+            <div className="w-full grid grid-flow-col grid-rows-1 gap-1 items-center ">
                 {renderIndicators()}
             </div>
             {renderContent()}
