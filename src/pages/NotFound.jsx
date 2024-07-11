632c764ad912d08654c80a7de380427592201eac
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-11 13:39:06 +0800

    Created the 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <div className="w-screen h-screen flex flex-col items-center justify-center gap-y-6 bg-brand-800">
+            <img className="w-2/5" src="./404.svg" />
+            <h1 className="text-white text-4xl">404 | Not Found</h1>
+            <h2 className="text-white text-2xl">The page you are trying to access doesn’t exist or has been moved.</h2>
+        </div>
+    );
+};
 
-export default NotFound
+export default NotFound;
