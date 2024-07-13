d0ae930ed0860effdf032639304c30968e2dc508
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-13 21:41:07 +0800

    Improved the news section in the home page

--- src/components/Home/News/index.jsx
+++ src/components/Home/News/index.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import NewsItem from "@/components/common/NewsItem";
+
+const items = [
+    {
+        title: "Title 1",
+        content: "Content 1",
+        date: "Date 1",
+    },
+    {
+        title: "Title 2",
+        content: "Content 2",
+        date: "Date 2",
+    },
+    {
+        title: "Title 3",
+        content: "Content 3",
+        date: "Date 3",
+    },
+    {
+        title: "Title 4",
+        content: "Content 4",
+        date: "Date 4",
+    },
+    {
+        title: "Title 5",
+        content: "Content 5",
+        date: "Date 5",
+    },
+]
 
 const News = () => {
-    return <div className="w-full h-full p-4">News</div>;
+    return (
+        <div className="w-full h-full p-4 grid grid-rows-[10%_90%]">
+            <p className="text-sm">News</p>
+            <div className="w-full h-full py-1 overflow-y-auto flex flex-col pr-2">
+               {items.map((item, index) => (
+                    <NewsItem key={index} newsItem={item} />
+                ))    
+            }
+            </div>
+        </div>
+    );
 };
 
 export default News;
--- src/components/common/ProjectCard.jsx
+++ src/components/common/ProjectCard.jsx
@@ -8,7 +8,7 @@ import {
 
 const ProjectCard = ({ project }) => {
     return (
-        <div className="w-full h-full p-2 flex flex-col gap-y-6 justify-evenly items-center border-[1px] border-gray-light rounded-lg shadow-sm">
+        <div className="w-full h-full p-2 flex flex-col gap-y-5 justify-evenly items-center border-[1px] border-gray-light rounded-lg shadow-sm">
             <h3 className="text-brand-800 text-sm">{project.name}</h3>
             <div className="w-full flex items-center justify-evenly">
                 <p className="flex flex-col items-center text-sm text-gray-med">
@@ -25,7 +25,7 @@ const ProjectCard = ({ project }) => {
                 </p>
             </div>
             <p className="text-sm text-gray-med">By: {project.owner}</p>
-            <button className="flex items-center justify-center rounded-md transition-all bg-brand-800 p-2 border-2 text-white hover:text-brand-800 md:transition-all md:hover:bg-white md:border-brand-800">
+            <button className="flex px-2 items-center justify-center rounded-md transition-all bg-brand-800 p-1 border-2 text-white hover:text-brand-800 md:transition-all md:hover:bg-white md:border-brand-800">
                 View Project
             </button>
         </div>
