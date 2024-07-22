41cf83745c13d489d7f02619c936e28aaed95ecc
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-22 12:25:03 +0800

    Changed the gap in the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ import News from "@/components/Home/News";
 
 const Home = () => {
     return (
-        <div className="w-full h-full overflow-hidden grid grid-cols-[65%_35%] grid-rows-2 gap-[2px] bg-gray-100 [&>*]:bg-white">
+        <div className="w-full h-full overflow-hidden grid grid-cols-[65%_35%] grid-rows-2 gap-[1px] bg-gray-100 [&>*]:bg-white">
             <Revenue />
             <Portfolio />
             <Projects />
