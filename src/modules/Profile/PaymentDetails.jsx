68ec79db60d4329ef8a136341c6b63676d2704a0
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-30 16:21:22 +0800

    Created a EmptyState component

--- src/modules/Profile/PaymentDetails.jsx
+++ src/modules/Profile/PaymentDetails.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import EmptyState from "@/components/common/EmptyState";
+import { faBank } from "@fortawesome/free-solid-svg-icons";
 
 const PaymentDetails = () => {
-    return <div>PaymentDetails</div>;
+    return (
+        <div className="w-full">
+            <EmptyState
+                title={"No Bank Accounts"}
+                description="Please add a bank account to receive payments."
+                icon={faBank}
+                buttonLabel="Add Bank Account"
+                handleClick={() => console.log("Add Bank Account")}
+            />
+        </div>
+    );
 };
 
 export default PaymentDetails;
