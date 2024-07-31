3854e3cc5b563d492c5aacad068d7caf465a3d7e
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-07-31 13:05:24 +0800

    Refactored the Status tab in the Project page to use teh Steps component

--- src/components/ProjectDetails/Status.jsx
+++ src/components/ProjectDetails/Status.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import RevenueGenerated from "@/components/ProjectDetails/RevenueGenerated";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faC, faCheck, faCircle } from "@fortawesome/free-solid-svg-icons";
+import Steps from "@/components/common/Steps";
 
-const statuses = [
+const STATUSES = [
     {
         title: "Planning",
         description: "Project is in the planning stage. Details are being fleshed out.",
@@ -23,49 +21,15 @@ const statuses = [
 ];
 
 const Status = ({ status }) => {
-    const isPastStatus = (index) => {
-        return index < statuses.findIndex((item) => item.title === status.current);
-    };
-
     return (
         <>
             <section className="pl-24">
                 <h2 className="text-3xl py-4 font-semibold capatalize">Steps</h2>
-                <div>
-                    <ol className="h-full flex flex-col gap-2 text-md font-medium text-gray-500 sm:gap-4">
-                        {statuses.map((item, index) => (
-                            <li key={item.title} className="relative mt-4 flex items-center">
-                                <div
-                                    className={`${index + 1 == Object.keys(statuses).length && "hidden"} ${isPastStatus(index) ? "bg-green-600" : "bg-gray-300"} absolute w-[1px] h-14 top-1/2 left-5`}
-                                />
-                                <span className="flex items-center justify-center mr-2 size-10 rounded-full bg-gray-50 text-center text-lg font-bold z-10">
-                                    {item.title === status.current ? (
-                                        <FontAwesomeIcon
-                                            icon={faCircle}
-                                            className="text-blue-600"
-                                        />
-                                    ) : isPastStatus(index) ? (
-                                        <FontAwesomeIcon
-                                            icon={faCheck}
-                                            className="text-green-600"
-                                        />
-                                    ) : null}
-                                </span>
-                                <span
-                                    className={`hidden sm:block ${
-                                        item.title === status.current
-                                            ? "text-blue-500"
-                                            : isPastStatus(index)
-                                              ? "text-green-600"
-                                              : "text-gray-500"
-                                    }`}
-                                >
-                                    {item.title}
-                                </span>
-                            </li>
-                        ))}
-                    </ol>
-                </div>
+
+                <Steps
+                    steps={STATUSES.map((step) => step.title)}
+                    currentStep={status.current}
+                />
             </section>
             <section className="pr-24">
                 <h2 className="text-3xl py-4 font-semibold capatalize">Updates</h2>
