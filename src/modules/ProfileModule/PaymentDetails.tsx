b26aa3af64f14c8c8c5b47157e4b7f8f1b072979
Mhmd-S <65135074+Mhmd-S@users.noreply.github.com>
2024-10-06 14:05:58 +0800

    Fixes to the personal information tab

--- src/modules/ProfileModule/PaymentDetails.tsx
+++ src/modules/ProfileModule/PaymentDetails.tsx
@@ -1,88 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import EmptyState from "@components/common/EmptyState";
+import { useAuthContext } from "@context/AuthContext";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBank, faUser, faCheck, faRemove } from "@fortawesome/free-solid-svg-icons";
 
-// ToDo: Maybe modularize it?
-// ToDo: If there is only one bank account, the remove button should be disabled.
-// ToDo: Change to bank details instead of payment details
-
 const PaymentDetails = () => {
+
+    const [editBank, setEditBank] = useState(false);
+
+    const { state } = useAuthContext();
+
     return (
         <div className="w-full">
             <h2 className="text-3xl py-4 font-semibold">Payment Details</h2>
-            {/* ToDo: Render if no state found */}
-            {/* <div className="mt-16"> 
-                <EmptyState
-                    title={"No Bank Accounts"}
-                    description="Please add a bank account to receive payments."
-                    icon={faBank}
-                    buttonLabel="Add Bank Account"
-                    handleClick={() => console.log("Add Bank Account")}
-                />
-            </div> */}
-            <ol className="w-full h-80 p-3 grid grid-cols-3 gap-3 border border-gray-200 rounded-lg">
-                <li className="w-full bg-[rgb(249,250,251)] grid grid-rows-[30%_45%_25%] grid-cols-1 rounded-md border-[1px] shadow-sm divide-y divide-gray-300">
-                    <div className="relative px-4 flex flex-col justify-center">
-                        <dt className="text-sm font-semibold">Account Number</dt>
-                        <dd>DE68500105178297336485</dd>
-                        <FontAwesomeIcon
-                            icon={faRemove}
-                            className="absolute right-2 top-2 text-red-500 cursor-pointer"
-                        />
-                    </div>
-                    <div className="h-full px-4 flex flex-col justify-center gap-3 text-sm text-gray-500">
-                        <text>
-                            <FontAwesomeIcon icon={faBank} className="mr-3" />
-                            MayBank
-                        </text>
-                        <text>
-                            <FontAwesomeIcon icon={faUser} className="mr-3" />
-                            John Doe
-                        </text>
-                        <text>
-                            <FontAwesomeIcon icon={faBank} className="mr-3" />
-                            MayBank
-                        </text>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <button className="w-1/3 bg-brand-800 text-white py-2 border border-brand-800 rounded-md transition-colors hover:bg-white hover:text-brand-800">
-                            Select
-                        </button>
-                    </div>
-                </li>
-                <li className="w-full bg-[rgb(249,250,251)] grid grid-rows-[30%_45%_25%] grid-cols-1 rounded-md border-[1px] shadow-sm divide-y divide-gray-300">
-                    <div className="px-4 flex flex-col justify-center">
-                        <dt className="text-sm font-semibold">Account Number</dt>
-                        <dd>DE68500105178297336485</dd>
-                    </div>
-                    <div className="h-full px-4 flex flex-col justify-center gap-3 text-sm text-gray-500">
-                        <text>
-                            <FontAwesomeIcon icon={faBank} className="mr-3" />
-                            MayBank
-                        </text>
-                        <text>
-                            <FontAwesomeIcon icon={faUser} className="mr-3" />
-                            John Doe
-                        </text>
-                        <text>
-                            <FontAwesomeIcon icon={faBank} className="mr-3" />
-                            MayBank
-                        </text>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <button className="w-1/3 text-brand-800 py-2 border border-brand-800 rounded-md cursor-not-allowed">
-                            Selected
-                            <FontAwesomeIcon icon={faCheck} className="ml-2" />
-                        </button>
-                    </div>
-                </li>
-                <li className="w-full flex flex-col items-center justify-center rounded-md border-[1px] shadow-sm">
-                    <button className="w-1/3  py-2 border bg-white text-brand-800 border-brand-800 rounded-md transition-colors hover:bg-brand-800 hover:text-white">
-                        Add
-                    </button>
-                </li>
-            </ol>
+
+            {!state.current.bankAccount ? (
+                <div className="mt-16">
+                    <EmptyState
+                        title={"No Bank Accounts"}
+                        description="Please add a bank account to receive payments."
+                        icon={faBank}
+                        buttonLabel="Add Bank Account"
+                        handleClick={() => console.log("Add Bank Account")}
+                    />
+                </div>
+            ) : (
+                <ol className="w-full h-80 p-3 grid grid-cols-3 gap-3 border border-gray-200 rounded-lg">
+                    <li className="w-full bg-[rgb(249,250,251)] grid grid-rows-[30%_45%_25%] grid-cols-1 rounded-md border-[1px] shadow-sm divide-y divide-gray-300">
+                        <div className="relative px-4 flex flex-col justify-center">
+                            <dt className="text-sm font-semibold">Account Number</dt>
+                            <dd>DE68500105178297336485</dd>
+                            <FontAwesomeIcon
+                                icon={faRemove}
+                                className="absolute right-2 top-2 text-red-500 cursor-pointer"
+                            />
+                        </div>
+                        <div className="h-full px-4 flex flex-col justify-center gap-3 text-sm text-gray-500">
+                            <text>
+                                <FontAwesomeIcon icon={faBank} className="mr-3" />
+                                MayBank
+                            </text>
+                            <text>
+                                <FontAwesomeIcon icon={faUser} className="mr-3" />
+                                John Doe
+                            </text>
+                            <text>
+                                <FontAwesomeIcon icon={faBank} className="mr-3" />
+                                MayBank
+                            </text>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <button className="w-1/3 bg-brand-800 text-white py-2 border border-brand-800 rounded-md transition-colors hover:bg-white hover:text-brand-800">
+                                Edit
+                            </button>
+                        </div>
+                    </li>
+                </ol>
+            )}
         </div>
     );
 };
--- src/modules/ProfileModule/PersonalDetails.tsx
+++ src/modules/ProfileModule/PersonalDetails.tsx
@@ -1,44 +1,45 @@
 import React from "react";
+import { useAuthContext } from "@context/AuthContext";
+import { normalizeCamelCase } from "@utils/extractHeader";
 
 const PersonalDetails = () => {
+    const { state } = useAuthContext();
+
+    const renderUserInfo = () => {
+        let userInfo = state.current;
+
+        if (state.current.userType === "Corporation") {
+            const representative = JSON.parse(userInfo.representative);
+
+            userInfo = {
+                ...userInfo,
+                ...representative,
+            };
+
+            delete userInfo.representative;
+        }
+
+        const {_id, password, userType, verified, createdAt, updatedAt, __v, addressProof, businessLicense, frontId, backId, selfieId, ...rest} = userInfo;
+
+        let userEntries = Object.entries(rest);
+
+        return userEntries.map(([key, value]) => (
+            <div key={key} className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt className="text-sm font-medium leading-6 text-gray-900">
+                    {normalizeCamelCase(key)}
+                </dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                    {value}
+                </dd>
+            </div>
+        ));
+    };
+
     return (
         <div>
             <h2 className="text-3xl py-4 font-semibold">Personal Information</h2>
             <div className="border-t border-gray-100">
-                <dl className="divide-y divide-gray-100">
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                            Margot Foster
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">
-                            Date of Birth
-                        </dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                            31/12/2002
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Address</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                            31, Subnag Jaya, Kuala Lumpur, Malaysia
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Nationality</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                            Malaysian
-                        </dd>
-                    </div>
-                    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                        <dt className="text-sm font-medium leading-6 text-gray-900">Phone</dt>
-                        <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                            +60 1 8 2975619
-                        </dd>
-                    </div>
-                </dl>
+                <dl className="divide-y divide-gray-100">{renderUserInfo()}</dl>
             </div>
         </div>
     );
